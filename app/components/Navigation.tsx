0670c537ee44987ce53f317e5cf99a3f9c3b1bca
sia <parkmy722698@gmail.com>
2025-04-14 19:05:27 +0900

    Feat. add Navigation Menu

---
+++ app/components/Navigation.tsx
@@ -0,0 +1,185 @@
+import { Link } from "react-router";
+import { Separator } from "./ui/separator";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuContent,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import { NavigationMenuLink } from "@radix-ui/react-navigation-menu";
+import { cn } from "../lib/utils";
+
+const menus = [
+  {
+    name: "Products",
+    to: "/products",
+    items: [
+      {
+        name: "Leaderboards",
+        description: "See the top performers in your community",
+        to: "/products/leaderboards",
+      },
+      {
+        name: "Categories",
+        description: "See the categories in our community",
+        to: "/products/categories",
+      },
+      {
+        name: "Search",
+        description: "Search for products",
+        to: "/products/search",
+      },
+      {
+        name: "Submit a Product",
+        description: "Submit a product to our community",
+        to: "/products/submit",
+      },
+      {
+        name: "Promote",
+        description: "Promote your product to our community",
+        to: "/products/promote",
+      },
+    ],
+  },
+  {
+    name: "Jobs",
+    to: "/jobs",
+    items: [
+      {
+        name: "Remote Jobs",
+        description: "Find a remote job in our community",
+        to: "/jobs?location=remote",
+      },
+      {
+        name: "Full-Time Jobs",
+        description: "Find a Full-Time job in our community",
+        to: "/jobs?type=full-time",
+      },
+      {
+        name: "Freelance Jobs",
+        description: "Find a Freelance job in our community",
+        to: "/jobs?type=full-time",
+      },
+      {
+        name: "Internships",
+        description: "Find an Internships in our community",
+        to: "/jobs?type=internship",
+      },
+      {
+        name: "Submit a Job",
+        description: "Submit a job in our community",
+        to: "/jobs/submit",
+      },
+    ],
+  },
+  {
+    name: "Community",
+    to: "/community",
+    items: [
+      {
+        name: "All Posts",
+        description: "Join all posts in our community",
+        to: "/community",
+      },
+      {
+        name: "Top Posts",
+        description: "Join all posts in our community",
+        to: "/community?sort=top",
+      },
+      {
+        name: "New Posts",
+        description: "Join all posts in our community",
+        to: "/community?sort=new",
+      },
+      {
+        name: "Create a Post",
+        description: "Create a post in our community",
+        to: "/community/create",
+      },
+    ],
+  },
+  {
+    name: "IdeasGPT",
+    to: "/ideas",
+  },
+  {
+    name: "Teams",
+    to: "/teams",
+    items: [
+      {
+        name: "All Teams",
+        description: "Join all teams in our community",
+        to: "/teams",
+      },
+      {
+        name: "Create a Team",
+        description: "Create a team in our community",
+        to: "/teams/create",
+      },
+    ],
+  },
+];
+
+export default function Navigation() {
+  return (
+    <nav className='flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50'>
+      <div className='flex items-center gap-4'>
+        <Link to='/' className='font-bold tracking-tighter text-lg'>
+          wemake
+        </Link>
+        <Separator orientation='vertical' className='h-6 mx-4' />
+        <NavigationMenu>
+          <NavigationMenuList>
+            {menus.map((menu) => (
+              <NavigationMenuItem key={menu.name}>
+                {menu.items ? (
+                  <>
+                    <Link to={menu.to}>
+                      <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+                    </Link>
+                    <NavigationMenuContent>
+                      <ul className='grid w-[400px] font-light gap-3 p-4 grid-cols-2'>
+                        {menu.items?.map((item) => (
+                          <NavigationMenuItem
+                            key={item.name}
+                            className={cn(
+                              "select-none rounded-md transition-colors focus:bg-accent hover:bg-accent",
+                              item.to === "/products/promote" &&
+                                "col-span-2 bg-primary/10 hover:bg-primary/20 focus:bg-primary/20",
+                              item.to === "/products/submit" &&
+                                "bg-primary/10 hover:bg-primary/20 focus:bg-primary/20"
+                            )}
+                          >
+                            <NavigationMenuLink asChild>
+                              <Link
+                                className='p-3 space-y-1 block loading-none no-underline outline-none'
+                                to={item.to}
+                              >
+                                <span className='text-sm font-medium leading-none'>
+                                  {item.name}
+                                </span>
+                                <p className='text-sm text-muted-foreground'>
+                                  {item.description}
+                                </p>
+                              </Link>
+                            </NavigationMenuLink>
+                          </NavigationMenuItem>
+                        ))}
+                      </ul>
+                    </NavigationMenuContent>
+                  </>
+                ) : (
+                  <Link to={menu.to} className={navigationMenuTriggerStyle()}>
+                    {menu.name}
+                  </Link>
+                )}
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </nav>
+  );
+}
--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import Navigation from "./components/Navigation";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -25,10 +26,10 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
@@ -42,7 +43,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+      <Navigation />
+      <Outlet />
+    </>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
@@ -62,11 +68,11 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
+    <main className='pt-16 p-4 container mx-auto'>
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+        <pre className='w-full p-4 overflow-x-auto'>
           <code>{stack}</code>
         </pre>
       )}
