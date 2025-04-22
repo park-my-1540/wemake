2a5d4d0b5ce6c53c5abee739a2a705026f557cfe
sia <parkmy722698@gmail.com>
2025-04-22 13:40:27 +0900

    Feat. add chart

--- app/features/users/pages/dashboard-page.tsx
+++ app/features/users/pages/dashboard-page.tsx
@@ -1,7 +1,73 @@
+import { Line } from "recharts";
+import { CartesianGrid, XAxis } from "recharts";
+import { LineChart } from "recharts";
 import type { Route } from "./+types/dashboard-page";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "~/components/ui/chart";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+
+const chartData = [
+  { month: "January", views: 186 },
+  { month: "February", views: 305 },
+  { month: "March", views: 237 },
+  { month: "April", views: 73 },
+  { month: "May", views: 209 },
+  { month: "June", views: 214 },
+];
+const chartConfig = {
+  views: {
+    label: "👁️",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
 
 export const meta: Route.MetaFunction = () => [{ title: "Dashboard" }];
 
 export default function DashboardPage() {
-  return <div></div>;
+  return (
+    <div className='space-y-5 h-full'>
+      <h1 className='text-2xl font-semibold mb-6'>Dashboard</h1>
+      <Card className='w-1/2'>
+        <CardHeader>
+          <CardTitle>Product Analytics</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey='month'
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Line
+                dataKey='views'
+                type='natural'
+                stroke='var(--color-views)'
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- app/features/users/pages/dashboard-product-page.tsx
+++ app/features/users/pages/dashboard-product-page.tsx
@@ -1,9 +1,89 @@
-import type { Route } from "./+types/dashboard-product-page";
+import { Area, AreaChart, Line } from "recharts";
+import { CartesianGrid, XAxis } from "recharts";
+import { LineChart } from "recharts";
+import type { Route } from "./+types/dashboard-page";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from "~/components/ui/chart";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 
-export const meta: Route.MetaFunction = ({ params }) => [
-  { title: `Product: ${params.productId}` },
+const chartData = [
+  { month: "January", views: 186, visitors: 100 },
+  { month: "February", views: 305, visitors: 33 },
+  { month: "March", views: 237, visitors: 300 },
+  { month: "April", views: 73, visitors: 200 },
+  { month: "May", views: 209, visitors: 500 },
+  { month: "June", views: 214, visitors: 100 },
 ];
+const chartConfig = {
+  views: {
+    label: "Page Views",
+    color: "hsl(var(--chart-1))",
+  },
+  visitors: {
+    label: "Visitors",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig;
+
+export const meta: Route.MetaFunction = () => [{ title: "Dashboard" }];
 
 export default function DashboardProductPage() {
-  return <div></div>;
+  return (
+    <div className='space-y-5 h-full'>
+      <h1 className='text-2xl font-semibold mb-6'>Analytics</h1>
+      <Card className='w-1/2'>
+        <CardHeader>
+          <CardTitle>Performance</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <AreaChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey='month'
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip
+                cursor={false}
+                wrapperStyle={{
+                  minWidth: "150px",
+                }}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Area
+                dataKey='views'
+                type='natural'
+                stroke='var(--color-views)'
+                fill='var(--color-views)'
+                strokeWidth={2}
+                dot={false}
+              />
+              <Area
+                dataKey='visitors'
+                type='natural'
+                stroke='var(--color-visitors)'
+                fill='var(--color-visitors)'
+                strokeWidth={2}
+                dot={false}
+              />
+            </AreaChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
