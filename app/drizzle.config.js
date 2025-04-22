dcc6a20062d3bdc28bfe43a05788b595037377e4
sia <parkmy722698@gmail.com>
2025-04-22 20:13:01 +0900

    chore. init drizzle orm

---
+++ app/drizzle.config.js
@@ -0,0 +1,12 @@
+// drizzle kit이 정보를 확인할 때 참조하는 파일
+
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./app/features/**/db/schema.ts", // 이 폴더 안에 있는 모든 파일을 참조 분할 방식
+  out: "./app/migrations", //drizzle kit이 생성한 sql 파일을 저장할 경로 뭔가 변경된점을 감지 하고 그때마다 sql 파일을 생성해줌 여기에 그 결과물
+  dialect: "postgresql", 
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
