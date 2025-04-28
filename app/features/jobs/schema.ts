ebd3857dce853f9efa4d24a00bb7cd663f36a3f1
sia <parkmy722698@gmail.com>
2025-04-28 10:51:08 +0900

    Feat. create job schema

---
+++ app/features/jobs/schema.ts
@@ -0,0 +1,34 @@
+import { bigint, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { JOB_TYPES, JOB_LOCATIONS, SALARY_RANGES } from "./constants";
+
+export const jobTypes = pgEnum(
+  "job_types",
+  JOB_TYPES.map((type) => type.name) as [string, ...string[]]
+);
+
+export const jobLocations = pgEnum(
+  "job_locations",
+  JOB_LOCATIONS.map((location) => location.name) as [string, ...string[]]
+);
+
+export const salaryRanges = pgEnum("salary_ranges", SALARY_RANGES);
+
+// snake case로 db column으로 쓰임
+export const jobs = pgTable("jobs", {
+  job_id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
+  position: text().notNull(),
+  overview: text().notNull(),
+  responsibilities: text().notNull(),
+  qualifications: text().notNull(),
+  benefits: text().notNull(),
+  skills: text().array().notNull(),
+  company_name: text().notNull(),
+  company_logo: text().notNull(),
+  company_location: text().notNull(),
+  company_url: text().notNull(),
+  job_type: jobTypes().notNull(),
+  job_location: jobLocations().notNull(),
+  salary_range: salaryRanges().notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+  updated_at: timestamp("updated_at").notNull().defaultNow(),
+});
