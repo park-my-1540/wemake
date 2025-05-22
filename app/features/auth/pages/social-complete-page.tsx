fe0edf0978a2312f311871296155e45d658e84ef
sia <parkmy722698@gmail.com>
2025-05-22 19:53:37 +0900

    Feat. Social Login - github, kakao

--- app/features/auth/pages/social-complete-page.tsx
+++ app/features/auth/pages/social-complete-page.tsx
@@ -1,21 +1,34 @@
+import { z } from "zod";
 import type { Route } from "./+types/social-complete-page";
-import type { MetaFunction } from "react-router";
+import { redirect } from "react-router";
+import { makeSSRClient } from "~/supa-client";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "소셜 로그인 완료" },
-    { name: "description", content: "소셜 로그인이 완료되었습니다" },
-  ];
+const paramsSchema = z.object({
+  provider: z.enum(["github", "kakao"]),
+});
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+  const { success, data } = paramsSchema.safeParse(params);
+  if (!success) {
+    return redirect("/auth/login");
+  }
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+  if (!code) {
+    return redirect("/auth/login");
+  }
+  const { client, headers } = makeSSRClient(request);
+  const { error } = await client.auth.exchangeCodeForSession(code);
+  /**
+   * 사용자를 Github으로 보낸 다음, 사용자가 그것을 수락하면
+   * Github이 우리를 위해 코드를 생성해줌. code=....
+   * 이 코드는 완료페이지의 URL에 포함되어 우리에게 줌. 이 코드를 가져와서 사용자로 교환
+   * 코드를 세션으로 교환하기만 하면 됨.
+   *
+   * header도 넘겨야하는데 코드와 세션을 교환하면, 사용자를 바로 로그인 시킴
+   */
+  if (error) {
+    throw error;
+  }
+  return redirect("/", { headers });
 };
-
-export function loader({ request, params }: Route.LoaderArgs) {
-  return {
-    provider: params.provider,
-  };
-}
-
-export default function SocialCompletePage({
-  loaderData,
-}: Route.ComponentProps) {
-  return <div className='space-y-6'></div>;
-}
--- app/features/auth/pages/social-start-page.tsx
+++ app/features/auth/pages/social-start-page.tsx
@@ -1,5 +1,11 @@
 import type { Route } from "./+types/social-start-page";
+import { z } from "zod";
+import { redirect } from "react-router";
+import { makeSSRClient } from "~/supa-client";
 
+const paramsSchema = z.object({
+  provider: z.enum(["github", "kakao"]),
+});
 export function meta() {
   return [
     { title: "소셜 로그인" },
@@ -7,12 +13,31 @@ export function meta() {
   ];
 }
 
-export function loader({ request, params }: Route.LoaderArgs) {
-  return {
-    provider: params.provider,
-  };
-}
-
-export default function SocialStartPage({ loaderData }: Route.ComponentProps) {
-  return <div className='space-y-6'></div>;
-}
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+  const { success, data } = paramsSchema.safeParse(params);
+  if (!success) {
+    return redirect("/auth/login");
+  }
+  const { provider } = data;
+  const redirectTo = `http://localhost:5173/auth/social/${provider}/complete`;
+  const { client, headers } = makeSSRClient(request);
+  const {
+    data: { url },
+    error,
+  } = await client.auth.signInWithOAuth({
+    //생성된 클라이언트를 통해서 인증작업
+    provider,
+    options: {
+      redirectTo,
+    },
+  });
+  if (url) {
+    return redirect(url, { headers });
+  }
+  if (error) {
+    throw error;
+  }
+};
+// 이 쿠키는 브라우저가 Github과 로그인 과정을 시작했다는 걸 클라이언트에게 알려주는 역할
+// Github에서 돌아왔을때 supabase client와 같은 기기에서 Github으로 로그인 과정을 시작했다는걸 알수있음.
+// 같은 기기에서 로그인을 끝내기 위한 보안상의 이유
