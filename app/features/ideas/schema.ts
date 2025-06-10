78a202657dca5e7226af866a0fcb278b4ee5acb6
sia <parkmy722698@gmail.com>
2025-06-10 17:01:18 +0900

    Feat. Drizzle에서 RLS정책 작성

--- app/features/ideas/schema.ts
+++ app/features/ideas/schema.ts
@@ -6,21 +6,35 @@ import {
   integer,
   uuid,
   primaryKey,
+  pgPolicy,
 } from "drizzle-orm/pg-core";
 import { profiles } from "../users/schema";
+import { authUid, serviceRole } from "drizzle-orm/supabase";
+import { sql } from "drizzle-orm";
 
-export const gptIdeas = pgTable("gpt_ideas", {
-  gpt_idea_id: bigint({ mode: "number" })
-    .primaryKey()
-    .generatedAlwaysAsIdentity(),
-  idea: text().notNull(),
-  views: integer().notNull().default(0),
-  claimed_at: timestamp(),
-  claimed_by: uuid().references(() => profiles.profile_id, {
-    onDelete: "cascade", //프로필이 삭제되면 채택된 아이디어도 삭제됨
-  }),
-  created_at: timestamp().notNull().defaultNow(),
-});
+export const gptIdeas = pgTable(
+  "gpt_ideas",
+  {
+    gpt_idea_id: bigint({ mode: "number" })
+      .primaryKey()
+      .generatedAlwaysAsIdentity(),
+    idea: text().notNull(),
+    views: integer().notNull().default(0),
+    claimed_at: timestamp(),
+    claimed_by: uuid().references(() => profiles.profile_id, {
+      onDelete: "cascade", //프로필이 삭제되면 채택된 아이디어도 삭제됨
+    }),
+    created_at: timestamp().notNull().defaultNow(),
+  },
+  (table) => [
+    pgPolicy("gpt_ideas-insert-policy", {
+      for: "insert",
+      to: serviceRole,
+      as: "permissive",
+      withCheck: sql`true`,
+    }),
+  ]
+);
 
 export const gptIdeasLikes = pgTable(
   "gpt_ideas_likes",
